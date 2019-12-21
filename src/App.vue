8df88fea5dc421a7a90a25886bab15d89cccc464
dgzxv <dgzxv@qq.com>
2019-12-21 14:27:38 +0800

    黄裕：提交控制初始页面

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,17 @@
 <template>
   <div id="app">
     <div class="content">
-         <router-view/>
+
+         <router-view/>         
+ 
+
     </div>
+
+
+
      <van-tabbar v-model="active" route>
        <van-tabbar-item replace to="/" name="home" icon="star-o">首页</van-tabbar-item>
        <van-tabbar-item replace to="/my" name="my" icon="user-o">我的</van-tabbar-item>
-
       </van-tabbar>
   </div>
 </template>
@@ -16,10 +21,13 @@ export default {
   data(){
     return{
       active:""
+   
     }
   }
 }
 </script>
+
 <style scoped>
+
 
 </style>
--- src/pages/manager/Home.vue
+++ src/pages/manager/Home.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="home">
+  <div class="home" :style="backgroundDiv">
+      <img src="./images/a.jpg" style="width: auto; height: 40px;">
+   
     <van-nav-bar fixed :title="'首页'" />
+       <div class="button" >
+ 
+    <van-button type="primary">一路灯光</van-button>
+    <van-button type="primary">二路灯光</van-button>
+    <van-button type="primary">三路灯光</van-button>
+    <van-button type="primary">灯光全开</van-button>
+    <van-button type="primary">灯光全关</van-button>
+    <van-button type="danger">开启门锁</van-button>
+    <van-button type="primary">开启空调</van-button>
+    <van-button type="primary">打开窗帘</van-button>
+
+    </div>
+
   </div>
+
+
 </template>
 <script>
 import {mapState,mapActions,mapGetters} from 'vuex'
@@ -14,11 +31,19 @@ export default {
   },
   methods:{
 
+  },
+  data(){
+    return{
+      active:"",
+      backgroundDiv: { backgroundImage: 'url(' + require('./images/a.jpg') + ')'}
+    }
   }
 }
 </script>
 <style scoped>
 .home {
   padding-bottom: 60px;
+
 }
 </style>
+  
